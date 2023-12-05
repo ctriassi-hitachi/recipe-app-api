ce9de585b86f283451942851b4e39facf961c3d1
Carlos Triassi <ctriassi@hitachisolutions.com>
2023-12-05 10:17:44 -0800

    upgrade to psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+
+"""
+Calculator function
+"""
+
+def add(x,y):
+    """ Add x + y ands return results"""
+    return x + y
+
+def test_substract_numbers(x,y):
+    return x-y    
+
+def subtract(x,y):
+    return y-x
